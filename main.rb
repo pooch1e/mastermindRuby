bca76e8416e63158da4524dee596fe2383cd6094
Joel Kram <joelnskra@gmail.com>
2025-02-27 09:39:23 +0000

    Refactor Mastermind game code for improved readability and structure

--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
-#Mastermind
+# Mastermind
 
 # make a 4-digit random number between 0000 and 9999
 # in a loop:
@@ -8,88 +8,79 @@
 #     count how many digits are correct but in the wrong place
 #     print the counts
 
-
-#refractor into class
-#split into game/player
+# refractor into class
+# split into game/player
 class Game
-  def initialize()
+  def initialize
     @game_won = false
     @answer = nil
     @turn = 0
-    gen_answer()
+    gen_answer
   end
 
-  def gen_answer() 
-    #gen random number to guess
-  @answer = rand(0..9999).to_s.rjust(4, "0")
-  #ensure number is 4 digits
-  
-  #split to array so easy to iterate over
-  @answer_array = @answer.split("")
+  def gen_answer
+    # gen random number to guess
+    @answer = rand(0..9999).to_s.rjust(4, '0')
+    # ensure number is 4 digits
 
-  print @answer_array #answer for debug
+    # split to array so easy to iterate over
+    @answer_array = @answer.split('')
+
+    print @answer_array # answer for debug
   end
 
-
-#compare guess to answer
-def compare_guess_to_answer(guess) 
-  puts @turn
-  if guess == @answer_array
-    puts "correct"
-    @game_won = true
-  else
-    puts "false"
-    #iterate through guess + compare to answer
-    #plus count everytime number is different
-    count = 0
-    guess.each_with_index do |value, index|
-      if value == @answer_array[index]
-        count +=1
+  # compare guess to answer
+  def compare_guess_to_answer(guess)
+    puts @turn
+    if guess == @answer_array
+      puts 'correct'
+      @game_won = true
+      guess
+    else
+      puts 'false'
+      # iterate through guess + compare to answer
+      # plus count everytime number is different
+      count = 0
+      guess.each_with_index do |value, index|
+        count += 1 if value == @answer_array[index]
+        puts count
+        @game_won = false
+        @turn += 1
       end
-    puts count
-    @game_won = false
-    @turn += 1
     end
   end
 
+  class Player
+    def initialize
+      puts 'Enter your name'
+      name = gets.chomp
+      puts "Welcome #{name}!"
+    end
 
-#end class
-end
-
-class Player
-  def initialize()
-    puts "Enter your name"
-    name = gets.chomp
-    puts "Welcome #{name}!"
-  end
-
-  #ask for guess
-  def ask_for_guess()
-    puts "What is your guess?"
-    guess = gets.chomp
-    guess_arr = guess.to_s.split("")
-    #ensure guess is only 4 digits
-    while guess_arr.length != 4
-      puts "guess again"
+    # ask for guess
+    def ask_for_guess
+      puts 'What is your guess?'
       guess = gets.chomp
-      guess_arr = guess.to_s.split("")
+      guess_arr = guess.to_s.split('')
+      # ensure guess is only 4 digits
+      while guess_arr.length != 4
+        puts 'guess again'
+        guess = gets.chomp
+        guess_arr = guess.to_s.split('')
+      end
+      guess_arr
     end
-  return guess_arr
+
+    # end class
   end
-  
-#end class  
+
+  my_game = Game.new
+  my_player = Player.new
+
+  # make a guess
+  # loop?
+  until my_game.instance_variable_get(:@game_won)
+    guess = my_player.ask_for_guess
+    my_game.compare_guess_to_answer(guess)
+  end
 end
-
-myGame = Game.new
-myPlayer = Player.new
-
-#make a guess
-#loop?
-while !myGame.instance_variable_get(:@game_won)
-  guess = myPlayer.ask_for_guess
-  myGame.compare_guess_to_answer(guess)
-end
-end
-
-
-
